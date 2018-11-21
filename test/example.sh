aecd423487bb86d69157a3431ac52b7d88af25a0
Yutaka Nishimura <ytk.nishimura@gmail.com>
2018-11-22 01:36:04 +0900

    add docker setting

--- test/example.sh
+++ test/example.sh
@@ -2,16 +2,16 @@
 
 set -eux
 
-curl -v localhost:8080/data
+curl -v $TEST_HOST:8080/data
 
-curl -v -X DELETE localhost:8080/data/0
+curl -v -X DELETE $TEST_HOST:8080/data/0
 
-curl -v -X POST localhost:8080/data -H "Accept: application/json" -H "Content-type: application/json" -d '{"xs":[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],"ys":[1,6,17,34,57,86,121,162,209,262,321]}'
+curl -v -X POST $TEST_HOST:8080/data -H "Accept: application/json" -H "Content-type: application/json" -d '{"xs":[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],"ys":[1,6,17,34,57,86,121,162,209,262,321]}'
 
-curl -v localhost:8080/data | jq .
+curl -v $TEST_HOST:8080/data | jq .
 
-curl -v localhost:8080/data/0/const/3 | jq .
+curl -v $TEST_HOST:8080/data/0/const/3 | jq .
 
-curl -v localhost:8080/data/0/constn/3 | jq .
+curl -v $TEST_HOST:8080/data/0/constn/3 | jq .
 
-curl -v localhost:8080/data/0/pred/1 | jq .
+curl -v $TEST_HOST:8080/data/0/pred/1 | jq .
